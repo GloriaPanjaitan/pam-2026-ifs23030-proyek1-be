ce302d8e9a584ec2e72288006dc1389852d115f0
GloriaPanjaitan <gloriapanjaitan3012@gmail.com>
2026-03-23 13:57:24 +0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,28 +1,46 @@
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                                     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) NOT NULL,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     photo VARCHAR(255) NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
-);
+    );
 
 CREATE TABLE IF NOT EXISTS refresh_tokens (
-     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-     user_id UUID NOT NULL,
-     refresh_token TEXT NOT NULL,
-     auth_token TEXT NOT NULL,
-     created_at TIMESTAMP NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS todos (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                                              id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL,
-    title VARCHAR(100) NOT NULL,
-    description TEXT NOT NULL,
-    is_done BOOLEAN NOT NULL,
-    cover TEXT NULL,
+    refresh_token TEXT NOT NULL,
+    auth_token TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS categories (
+                                          id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL,
+    description TEXT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
-);
+    );
+
+CREATE TABLE IF NOT EXISTS articles (
+                                        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    thumbnail VARCHAR(255) NULL,
+    is_published BOOLEAN NOT NULL DEFAULT false,
+    category_id UUID NOT NULL,
+    author_id UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS comments (
+                                        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    content TEXT NOT NULL,
+    article_id UUID NOT NULL,
+    author_id UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+    );
