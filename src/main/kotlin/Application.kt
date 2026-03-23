7039c2f1fc3cf04e29ddd687253d310cf9c4c514
GloriaPanjaitan <gloriapanjaitan3012@gmail.com>
2026-03-23 19:04:35 +0700

    initdd

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -76,7 +76,33 @@ fun Application.module() {
     install(ContentNegotiation) {
         gson {
             setPrettyPrinting()
+            registerTypeAdapter(java.time.Instant::class.java, object : com.google.gson.TypeAdapter<java.time.Instant>() {
+                override fun write(out: com.google.gson.stream.JsonWriter, value: java.time.Instant?) {
+                    out.value(value?.toString())
+                }
+                override fun read(input: com.google.gson.stream.JsonReader): java.time.Instant? {
+                    return java.time.Instant.parse(input.nextString())
+                }
+            })
+        }GET https://pam-2026-ifs23030-proyek1-be.ifs23030.space:8080/users/me
+
+        HTTP/1.1 500 Internal Server Error
+        Server: nginx/1.28.0
+        Date: Mon, 23 Mar 2026 12:00:16 GMT
+        Content-Type: application/json
+        Content-Length: 297
+        Connection: keep-alive
+
+        {
+            "status": "error",
+            "message": "Failed making field 'java.time.Instant#seconds' accessible; either increase its visibility or write a custom TypeAdapter for its declaring type.\nSee https://github.com/google/gson/blob/main/Troubleshooting.md#reflection-inaccessible",
+            "data": ""
         }
+        Response file saved.
+        > 2026-03-23T190016.500.json
+
+        Response code: 500 (Internal Server Error); Time: 616ms (616 ms); Content length: 287 bytes (287 B)
+
     }
 
     install(Koin) {
